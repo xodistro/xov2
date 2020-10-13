3e356f65c64b7d1a11da9880c638b5d9bd4589a8
xodistro <60867479+xodistro@users.noreply.github.com>
2020-10-13 00:47:28 -0500

    Update help.lua

--- help.lua
+++ help.lua
@@ -1,1 +1,37 @@
 yeah so, this is comin soon when i feel like writing out all the commands kill me later lol
+command list for xo bot
+Prefix: , or /
+* = not required
+** = bot owner Only
+^ = server admin Only
+-------
+join xo discord if yu find bugs, or yu wanna vibe, or tell me my bot is trash, or tell me yu love my bot, its up to yu.
+if yu find urgent bugs that could destroy servers, dm me at xo#0002  
+discord invite: https://youngforever.tk/discord
+bot invite: youngforever.tk
+-------
+server setup:
+             info: yu might need these to setup your server...
+    1. (^)sets up moderation system           : /setme
+    2. (^)configures a muted or jailed role   : /configjr <@role> or /configmr <@<role>
+    3. (^)configures jailed channel           : /configjc <channel> 
+
+moderation:
+             info: the moderation commands. slam this shit lol
+    1. bans mentioned member                  : /ban <user> (reason)
+    2. kicks mentioned member                 : /kick <user> (reason)
+    3. mutes mentioned member                 : /mute <user> 
+    4. jails mentioned member                 : /jail <user> (reason)
+    5. image mutes mentioned member           : /imute <user>
+    6. reaction mutes mentioned member        : /rmute <user>
+    7. unmutes mentioned member               : /unmute <user>
+    8. unjails mentioned member               : /unjail <user>
+    9. un-image mutes member                  : /iunmute <user> (experimental, don't rely on)
+    10. un-reaction mutes member              : /runmute <user>
+    11. locks down the channel                : /lockdown
+    12. unlocks the channel                   : /unlock
+    13. (*) locks the channel                 : /exlock
+    14. (*) unlocks the channel               : /exunlock
+    15. (*) bans mentioned member             : /exban <user> (reason)
+    16. (*) kicks mentioned member            : /exkick <user> (reason)  
+        
