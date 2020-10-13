8604ae2afb69294d0246b9efc7126d303fd601ef
xodistro <60867479+xodistro@users.noreply.github.com>
2020-10-13 14:30:04 -0500

    Update help.lua

--- help.lua
+++ help.lua
@@ -1,6 +1,6 @@
 this is gradually being updat4ed. not all of the commands are here, only the importantish ones
 command list for xo bot
-Prefix: , or /
+Prefix: , 
 * = not required
 ** = bot owner Only
 ^ = server admin Only
@@ -12,26 +12,26 @@ bot invite: youngforever.tk
 -------
 server setup:
              info: yu might need these to setup your server...
-    1. (^)sets up moderation system           : /setme
-    2. (^)configures a muted or jailed role   : /configjr <@role> or /configmr <@<role>
-    3. (^)configures jailed channel           : /configjc <channel> 
+    1. (^)sets up moderation system           : ,setme
+    2. (^)configures a muted or jailed role   : ,configjr <@role> or /configmr <@<role>
+    3. (^)configures jailed channel           : ,configjc <channel> 
 
 moderation:
              info: the moderation commands. slam this shit lol
-    1. bans mentioned member                  : /ban <user> (reason)
-    2. kicks mentioned member                 : /kick <user> (reason)
-    3. mutes mentioned member                 : /mute <user> 
-    4. jails mentioned member                 : /jail <user> (reason)
-    5. image mutes mentioned member           : /imute <user>
-    6. reaction mutes mentioned member        : /rmute <user>
-    7. unmutes mentioned member               : /unmute <user>
-    8. unjails mentioned member               : /unjail <user>
-    9. un-image mutes member                  : /iunmute <user> (experimental, don't rely on)
-    10. un-reaction mutes member              : /runmute <user>
-    11. locks down the channel                : /lockdown
-    12. unlocks the channel                   : /unlock
-    13. (**) locks the channel                 : /exlock
-    14. (**) unlocks the channel               : /exunlock
-    15. (**) bans mentioned member             : /exban <user> (reason)
-    16. (**) kicks mentioned member            : /exkick <user> (reason)  
+    1. bans mentioned member                  : ,ban <user> (reason)
+    2. kicks mentioned member                 : ,kick <user> (reason)
+    3. mutes mentioned member                 : ,mute <user> 
+    4. jails mentioned member                 : ,jail <user> (reason)
+    5. image mutes mentioned member           : ,imute <user>
+    6. reaction mutes mentioned member        : ,rmute <user>
+    7. unmutes mentioned member               : ,unmute <user>
+    8. unjails mentioned member               : ,unjail <user>
+    9. un-image mutes member                  : ,iunmute <user> (experimental, don't rely on)
+    10. un-reaction mutes member              : ,runmute <user>
+    11. locks down the channel                : ,lockdown
+    12. unlocks the channel                   : ,unlock
+    13. (**) locks the channel                : ,exlock
+    14. (**) unlocks the channel              : ,exunlock
+    15. (**) bans mentioned member            : ,exban <user> (reason)
+    16. (**) kicks mentioned member           : ,exkick <user> (reason)  
         
