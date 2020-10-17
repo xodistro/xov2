6354bd1a8fbab6156a5b9d62b648f28c1eedd15d
xo <60867479+xodistro@users.noreply.github.com>
2020-10-17 18:53:37 -0500

    Update updates.lua

--- updates.lua
+++ updates.lua
@@ -7,5 +7,9 @@ bot game status changed to do not disturb/streaming love hate
 blacklist command has been made useful. ",blacklist <add | remove | check> (reason) for add
 blacklist command reason exploit patched
 unban still broken, working on it.
-when/if ,setme doesn't work correctly (roles not logging or channels not logging), use ,configmr or ,configjr for roles and ,configlc for jail channel.
+when/if ,setme does not work correctly (roles not logging or channels not logging), use ,configmr or ,configjr for roles and ,configlc for jail channel.
 ----End of Update Log 10/14/20----
+----Updates on 10/15/20----
+,unlock now has an alias. unlock or unlockdown now works.
+,lockdown now has an alias. lock or lockdowm no works.
+----End of Update Log 10/15/20----      
