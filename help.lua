f7b9d592eed57b17f02a9abd4c01395a2e2d5e6a
xo <60867479+xodistro@users.noreply.github.com>
2020-10-17 18:46:19 -0500

    Update help.lua

--- help.lua
+++ help.lua
@@ -1,6 +1,7 @@
 this is gradually being updated. not all of the commands are here, only the importantish ones
 command list for xo bot
 Prefix: , 
+if yu do not see some commands, its either because they have no use or i dont feel like putting them right now
 () = not required
 <> = required
 ** = bot owner Only
@@ -13,7 +14,8 @@ bot invite: https://youngforever.tk
 -------
 server setup:
              info: yu might need these to setup your server...
-    1. (^)sets up moderation system           : ,setme
+               --these kinda useful, might wanna use these--
+    1. (^)sets up moderation system           : ,setme --especially this one--
     2. (^)configures a muted or jailed role   : ,configjr <@role> or ,configmr <@<role>
     3. (^)configures jailed channel           : ,configjc <channel> 
     4. (^)creates jail if ,setme breaks       : ,createjail
@@ -29,8 +31,8 @@ moderation:
     6. reaction mutes mentioned member        : ,rmute <user>
     7. unmutes mentioned member               : ,unmute <user>
     8. unjails mentioned member               : ,unjail <user>
-    9. un-image mutes member                  : ,iunmute <user> (experimental, do not rely on)
-    10. un-reaction mutes member              : ,runmute <user> (experimental, do not rely on)
+    9. un-image mutes member                  : ,iunmute <user> --experimental, do not rely on--
+    10. un-reaction mutes member              : ,runmute <user> --experimental, do not rely on--
     11. locks down the channel                : ,lockdown
     12. unlocks the channel                   : ,unlock
     13. (**) locks the channel                : ,exlock
@@ -38,6 +40,34 @@ moderation:
     15. (**) bans mentioned member            : ,exban <user> (reason)
     16. (**) kicks mentioned member           : ,exkick <user> (reason)
     
-moderation:
+fun:
              info: fun things that have absolutely no background or reason behind them
-    1.        
+    1. gives command author the rapist role  : ,rape (side note, i do not condone this lmao)
+    2. gives mentioned user a gay role       : ,gay <@user>
+    3. gives mentioned user clown role       : ,clown <@user>
+    --to be added upon--          
+misc:
+             info: useful or useless things
+    1. sends a string of characters          : ,passwordgenerator <length>
+    2. checks if the bot is online           : ,check
+    3. do not go down the rabbit hole        : ,yes
+    4. sends a message if the bot is on      : ,status
+    5. sends mentioned user love             : ,ily <user>
+    6. sends avatar of mentioned user        : ,avatar (user)
+    7. changes nickname of mentioned user    : ,nickname (user)
+    --to be continuted too lmfao--
+                
+utility:
+             info: things yu may need who knows
+    1. adds a role to member                 : ,addrole <user> <role>
+    2. removes a role from member            : ,removerole <user> <role>
+    3. creates a role in the server          : ,createrole <role name> <hex code>            
+    4. deletes a role in the server          : ,deleterole <role>
+    5. lists who has that role               : ,members
+    6. creates a permanent invite            : ,perminv
+    7. creates a poll                        : ,poll <question>
+    8. reacts with a thumbs up and down      : ,qp 
+    9. fake nuke message, lol                : ,nuke
+    10. sends user a message                 : ,dm <message>
+    11. sends an embed with rules            : ,rules
+                    
