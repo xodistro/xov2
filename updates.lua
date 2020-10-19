7b10b79dc65ffbe3897f871d62d3a3cb012f9772
xo <60867479+xodistro@users.noreply.github.com>
2020-10-19 02:38:42 -0500

    Update updates.lua

--- updates.lua
+++ updates.lua
@@ -6,8 +6,8 @@ passwords generated from password generator are not saved. i do not have access 
 bot game status changed to do not disturb/streaming love hate
 blacklist command has been made useful. ",blacklist <add | remove | check> (reason) for add
 blacklist command reason exploit patched
-unban still broken, working on it.
-when/if ,setme does not work correctly (roles not logging or channels not logging), use ,configmr or ,configjr for roles and ,configlc for jail channel.
+unban still broken, fixing it.
+when/if ,setme breaks (roles not logging or channels not logging), use ,configmr or ,configjr for roles and ,configlc for jail channel.
 ----End of Update Log 10/14/20----
 ----Updates on 10/15/20----
 ,unlock now has an alias. unlock or unlockdown can be used.
