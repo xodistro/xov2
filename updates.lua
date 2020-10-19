429314d53105955b53d88b829acbb1c750d34026
xo <60867479+xodistro@users.noreply.github.com>
2020-10-19 02:41:48 -0500

    Update updates.lua

--- updates.lua
+++ updates.lua
@@ -1,10 +1,10 @@
 ----Updates on 10/14/20----
 problem with ,setme not logging mute role id fixed
-mute command finally fixed ",mute <user> reason is no longer required
-password generator command ",passwordgenerator <length>"
+mute command finally fixed. no more reason breaks
+password generator command = ,passwordgenerator <length>
 passwords generated from password generator are not saved. i do not have access to them.
 bot game status changed to do not disturb/streaming love hate
-blacklist command has been made useful. ",blacklist <add | remove | check> (reason) for add
+blacklist command has been made useful. = ,blacklist <add | remove | check> (reason) for add
 blacklist command reason exploit patched
 unban still broken, fixing it.
 when/if ,setme breaks (roles not logging or channels not logging), use ,configmr or ,configjr for roles and ,configlc for jail channel.
